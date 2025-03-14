ab8f631de550bf7fe05f9efa4d7ed1e28f451465
Thenu Kaluarachchi <thenujaya123@gmail.com>
2025-03-14 19:01:33 +0000

    updates

--- CoolJazz/settings.py
+++ CoolJazz/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MAIN', 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'MAIN', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'MAIN', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
